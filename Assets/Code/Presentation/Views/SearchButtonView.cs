6976885371700b38519dd092131871b5b3d520c9
Ausias Dalmau <ausiasdalmauroig@gmail.com>
2020-08-24 20:24:21 +0200

    Search field UI

---
+++ Assets/Code/Presentation/Views/SearchButtonView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchButtonView : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+    [SerializeField] private Animator _animator;
+
+    private bool _searching = false;
+    private int _searchingHash = Animator.StringToHash("Searching");
+    private int _directionHash = Animator.StringToHash("Direction");
+    void Start()
+    {
+        _button.onClick.AddListener(SearchButtonPressed);   
+    }
+
+    private void SearchButtonPressed()
+    {
+        _searching = !_searching;
+        
+        _animator.SetBool(_searchingHash, _searching);
+        
+        float direction = 1.0f;
+        if (!_searching)
+        {
+            direction = -1.0f;
+        }
+        
+        _animator.SetFloat(_directionHash, direction);
+    }
+}
